7fdcea94115448e353c7278f902dceaa7b8811f4
Shoyur <100440995+Shoyur@users.noreply.github.com>
2023-10-26 09:36:19 -0400

    ...

---
+++ 25_hash-table.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+int main() {
+
+    // hash table
+
+    // disons une linked list, on doit chercher au travers
+    // et qu'elle contient 10,000 éléments,
+    // la recherhce pourrait être trop longue
+
+    // idéalement la recherche devrait être O(1)
+    // au lieu de O(n)
+
+    // en Python, le dict est en fait un hash table
+
+    
+
+    return 0;
+}
